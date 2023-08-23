27769038fdf8efe49007f99ef644cb0c0e2acf18
alaodriss <alaodriss@gmail.com>
2023-08-23 17:19:07 +0200

    add header

--- src/compoments/header/Header.jsx
+++ src/compoments/header/Header.jsx
@@ -1,45 +1,72 @@
-import React, { useState } from 'react'
-import logo from '../../assets/images/logo-notagline.svg'
-import { BsSearch } from 'react-icons/bs';
-import './Header.css'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import logo from "../../assets/images/logo-notagline.svg";
+import { BsSearch } from "react-icons/bs";
+import "./Header.css";
 
 function Header() {
+  const [isSearchVisible, setSearchVisible] = useState(false);
 
-    const [isSearchVisible, setSearchVisible] = useState(false);
-
-    const toggleSearch = () => {
-        setSearchVisible(!isSearchVisible);
-      };
+  const toggleSearch = () => {
+    setSearchVisible(!isSearchVisible);
+  };
 
   return (
     <>
-      <div className='container'>
-            <div className='header-hero'>
-                   <div className='header-logo'>
-                        <img src={logo} alt='logo' />
-                        <p className='number-phone'>01 45 67 34 22 </p>
-                    </div> 
-                    <div className='header-inscription'>
-                    <div className="bar-search">
-                            <button className="icon-search" onClick={toggleSearch}>
-                            <BsSearch />
-                            </button>
-                            {isSearchVisible && (
-                                <form>
-                                <input type="search" placeholder="Search..." />
-                                </form>
-                            )}
-                            </div>
-                        <button className='btn-inscription'>Souscrire en ligne</button>
-                        <button className='btn-client'>Espace client</button>
-                    </div>
+      <div className="container">
+        <div className="header-hero">
+          <div className="header-logo">
+            <img src={logo} alt="logo" />
+            <p className="number-phone">01 45 67 34 22 </p>
+          </div>
+          <div className="header-inscription">
+            <div className="bar-search">
+              <button className="icon-search" onClick={toggleSearch}>
+                <BsSearch />
+              </button>
+              {isSearchVisible && (
+                <form>
+                  <input type="search" placeholder="Search..." />
+                </form>
+              )}
             </div>
-            <div className='menus'>
-            
-            </div>
+            <button className="btn-inscription">Souscrire en ligne</button>
+            <button className="btn-client">Espace client</button>
+          </div>
+        </div>
+        <div className="menus">
+          <ul className="bottom-navigation-bar">
+            <li>
+              <a exact to="/">
+                Assurance vie
+              </a>
+            </li>
+            <li>
+              <a to="/">Retraite – PER</a>
+            </li>
+            <li>
+              <a to="/">SCPI</a>
+            </li>
+            <li>
+              <a exact to="/">
+              Produits structurés
+              </a>
+            </li>
+            <li>
+              <a to="/">Autres placements</a>
+            </li>
+            <li>
+              <a to="/">Comprendre l’épargne</a>
+            </li>
+            <li>
+              <a to="/">
+                    Découvrez-nouss</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </>
-  )
+  );
 }
 
-export default Header
+export default Header;
